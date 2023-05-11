b4d029663ce392c56dbeb0157d3b398accd701bd
Yerim96 <chzh9993@naver.com>
2023-05-11 14:53:06 +0900

    first

---
+++ allergyPage2.js
@@ -0,0 +1,17 @@
+// //버튼 search 가져옴
+const search=document.querySelector('#search');
+
+// //modal 가져오기
+// const modal = document.querySelector('.mol');
+
+// //close 가져오기
+// const close = document.querySelector('.modla_closeBtn');
+
+
+search.addEventListener('click',function(){
+  alert('검색입니다');
+});
+
+
+
+
